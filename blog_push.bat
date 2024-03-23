944d6c0fe64e51dcbacaa8796700da0dda8c9b30
griedzx <griedzx@users.noreply.github.com>
2024-03-23 16:41:15 +0000

    Daily update 2024/03/24 周日 griedzx/my_website@8daf3456abbc949c35d80a687b980ed1eb277273

--- blog_push.bat
+++ blog_push.bat
@@ -8,15 +8,17 @@ set "outputFile=E:\my_website\static\blog.log"
 echo %date% >> %outputFile%
 
 :: 计算文件中的日期行数
-set "lineCount=0"
-for /f %%A in ('type "%outputFile%"^|find /c /v ""') do set "lineCount=%%A"
+set "dateCount=0"
+for /f "delims=" %%A in ('findstr /r "^[0-9][0-9]*-[0-9][0-9]*-[0-9][0-9]*" "%outputFile%"') do (
+    set /a "dateCount+=1"
+)
 
 :: 如果日期行数达到5，覆盖文件
-if !lineCount! geq 5 (
+if !dateCount! geq 5 (
     echo %date% > %outputFile%
 )
 
 cd E:\my_website\ >> %outputFile% 2>&1
 git add . >> %outputFile% 2>&1
 git commit -m "Daily update %date%" >> %outputFile% 2>&1
-git push origin source >> %outputFile% 2>&1
+git push origin source >> %outputFile% 
