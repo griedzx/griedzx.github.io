a77fe24ab6dffb3ffc484c49ed3fa1b0a3cd5b36
griedzx <griedzx@users.noreply.github.com>
2024-03-23 16:30:23 +0000

    Daily update 2024/03/24 周日 griedzx/my_website@c06a5b8601d346b1c042edc6228ece7f8c58ca67

--- blog_push.bat
+++ blog_push.bat
@@ -1,5 +1,22 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: 定义输出文件名
+set "outputFile=history.txt"
+
+:: 打印日期并重定向到文件
+echo %date% >> %outputFile%
+
+:: 计算文件中的日期行数
+set "lineCount=0"
+for /f %%A in ('type "%outputFile%"^|find /c /v ""') do set "lineCount=%%A"
+
+:: 如果日期行数达到5，覆盖文件
+if !lineCount! geq 5 (
+    echo %date% > %outputFile%
+)
+
 cd E:\my_website\
 git add .
-git commit -m "Daily update $(date)"
-git push origin source
+git commit -m "Daily update %date%"
+git push origin source >> %outputFile% 2>&1
